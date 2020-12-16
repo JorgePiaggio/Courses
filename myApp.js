e5c932611106fbaa3d02d686e065b508c46331b0
JorgePiaggio <proto2345@protonmail.com>
2020-12-15 21:32:25 -0300

    reqSendFile

--- myApp.js
+++ myApp.js
@@ -4,13 +4,15 @@ var app = express();
 
 console.log("Hello World");
 
-app.get("/", function(req, res) {
+/* app.get("/", function(req, res) {
   res.send("Hello Express");
-});
+}); */
 
 
 
-
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/views/index.html");
+ })
 
 
 
