55e12818c8ecbc7818601a1d9c4d76a8140a169e
JorgePiaggio <proto2345@protonmail.com>
2021-01-03 11:08:57 -0300

    model.findOneAndUpdate

--- MongoDB And Mongoose/myApp.js
+++ MongoDB And Mongoose/myApp.js
@@ -149,20 +149,37 @@ const findEditThenSave = (personId, done) => {
     if(err) return console.log(err);
     
     //add fav food
-    match.favoriteFoods.push(foodToAdd);
+      match.favoriteFoods.push(foodToAdd);
 
     //save and return upd obj
-    match.save((err, updatedPerson) => {
-      if(err) return console.log(err);
-      done(null, updatedPerson);
+      match.save((err, updatedPerson) => {
+        if(err) return console.log(err);
+        done(null, updatedPerson);
       });
   });
 };
 
+
+/*
+Perform New Updates on a Document Using model.findOneAndUpdate()
+
+Recent versions of Mongoose have methods to simplify documents updating. 
+Some more advanced features (i.e. pre/post hooks, validation) behave differently with this approach, 
+so the classic method is still useful in many situations. findByIdAndUpdate() can be used when searching by id.
+Modify the findAndUpdate function to find a person by Name and set the person's age to 20. 
+Use the function parameter personName as the search key.
+Note: You should return the updated document. To do that, you need to pass 
+the options document { new: true } as the 3rd argument to findOneAndUpdate(). 
+By default, these methods return the unmodified object.
+*/
+
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, match) => {
+    if(err) return console.log(err);
+    done(null, match);
+  })
 };
 
 const removeById = (personId, done) => {
