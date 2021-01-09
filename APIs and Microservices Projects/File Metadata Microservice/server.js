6475d1fee134dec5c6a8a253bfc7b5dd72527734
JorgePiaggio <proto2345@protonmail.com>
2021-01-09 20:23:45 -0300

    project complete

--- APIs and Microservices Projects/File Metadata Microservice/server.js
+++ APIs and Microservices Projects/File Metadata Microservice/server.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');  //middleware designed for handling the multipart/form-data // https://www.npmjs.com/package/multer
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(multer().single('upfile'));// upfile => input name
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post('/api/fileanalyse', (req, res) => {
+  console.log(req.file);
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+    encoding: req.file.encoding
+  });
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
