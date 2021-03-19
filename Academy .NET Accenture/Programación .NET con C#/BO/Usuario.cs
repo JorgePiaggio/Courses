687b64c78ac7b30808e580a66d64c21235d2fac0
JorgePiaggio <proto2345@protonmail.com>
2021-03-19 18:18:14 -0300

    winform - Login FirstProject

---
+++ Academy .NET Accenture/Programación .NET con C#/BO/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Usuario
+    {
+        #region Constructores
+        // constructor
+        public Usuario() { }
+        public Usuario(string nombre, string password, string email) 
+        {
+            Nombre = nombre;
+            Password = password;
+            Email =  email;
+        }
+        #endregion
+
+        #region propiedades auto implementadas
+        // propiedad auto implementada
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        #endregion
+
+
+    }
+}
