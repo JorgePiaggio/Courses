15669164112d95d53166b0888c143d0da774087d
JorgePiaggio <proto2345@protonmail.com>
2021-03-23 08:38:28 -0300

    css project add-ons

--- Academy .NET Accenture/Maquetación Web: HTML 5 y CSS/Laboratorio/js/codigo.js
+++ Academy .NET Accenture/Maquetación Web: HTML 5 y CSS/Laboratorio/js/codigo.js
@@ -1,7 +1,11 @@
 function abrir(){
     document.getElementById('formulario').style.display = 'block'
+    document.getElementById('content').style.filter = 'sepia(50%)'
 }
 
 function cerrar(){
     document.getElementById('formulario').style.display = 'none'
+    document.getElementById('content').style.filter = 'sepia(0%)'
 }
+
+cerrar();
