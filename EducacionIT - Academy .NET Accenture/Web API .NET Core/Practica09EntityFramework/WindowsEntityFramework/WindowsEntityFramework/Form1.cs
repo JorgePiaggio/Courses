d8fad44c0666bd208f71819dc6589bdcbcf3c3d6
JorgePiaggio <proto2345@protonmail.com>
2021-03-28 16:32:30 -0300

    EntityFramework practica 8,9

---
+++ EducacionIT - Academy .NET Accenture/Web API .NET Core/Practica09EntityFramework/WindowsEntityFramework/WindowsEntityFramework/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsEntityFramework.Data;
+using WindowsEntityFramework.Repository;
+
+namespace WindowsEntityFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MostrarAuthors();
+        }
+
+        private void MostrarAuthors()
+        {
+            gridAuthors.DataSource = AuthorRepository.GetList();
+        }
+
+        private void btnBuscarCiudad_Click(object sender, EventArgs e)
+        {
+            string city = txtCity.Text;
+            gridAuthors.DataSource = AuthorRepository.GetList(city);
+        }
+
+        private void btnBuscarAuthorId_Click(object sender, EventArgs e)
+        {
+            Author author = AuthorRepository.GetAuthor(txtAuthorId.Text);
+            MessageBox.Show(author.au_fname + " " + author.au_lname);
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            Author author = new Author()
+            {
+                au_fname = "Juan",
+                au_lname = "Perez",
+                au_id = "22-1242-323",
+                phone = "415-444-2122",
+                contract = true
+            };
+
+           int  result = AuthorRepository.Create(author);
+            MostrarAuthors();
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            Author author = new Author()
+            {
+                au_fname = "Juan Manuel",
+                au_lname = "Perez",
+                au_id = "22-1242-323",
+                phone = "415-333-2117",
+                contract = true
+            };
+
+            int result = AuthorRepository.Update(author);
+            MostrarAuthors();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Author author = new Author()
+            {
+                au_fname = "Juan Manuel",
+                au_lname = "Perez",
+                au_id = "22-1242-323",
+                phone = "415-333-2117",
+                contract = true
+            };
+
+            int result = AuthorRepository.Delete(author);
+            MostrarAuthors();
+        }
+    }
+}
---
+++ EducacionIT - Academy .NET Accenture/Web API .NET Core/Practica09EntityFramework/WindowsEntityFramework/WindowsEntityFramework/Repository/AuthorRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsEntityFramework.Data;
+
+namespace WindowsEntityFramework.Repository
+{
+    public static class AuthorRepository
+    {
+        private static PubsContext context = new PubsContext();
+
+
+        // context
+        public static List<Author> GetList()
+        {
+            return context.Authors.ToList();
+        }
+        // Linq to Entities ( -> queries ) 
+        public static List<Author> GetList(string city)
+        {
+            List<Author> autores = (from a in context.Authors where a.city == city select a).ToList();
+            return autores;
+        }
+        // Linq
+        public static Author GetAuthor(string authorId)
+        {
+            Author autor = (from a in context.Authors where a.au_id==authorId select a).Single();
+            return autor;
+        }
+
+        public static int Create(Author author)
+        {
+            context.Authors.Add(author); // agrega en memoria
+            int result = context.SaveChanges(); // guarda en la base
+            return result;
+        }
+        public static int Update(Author author)
+        {
+            Author a = context.Authors.Find(author.au_id);
+            int result = -1;
+
+            if ( a != null)
+            {
+                a.au_lname = author.au_lname;
+                a.au_fname = author.au_fname;
+                a.address = author.address;
+                a.city=author.city;
+                a.phone=author.phone;
+                a.state=author.state;
+                a.zip=author.zip;
+                a.contract= author.contract;
+                result = context.SaveChanges();
+            }
+
+            return result;
+        }
+
+        public static int Delete(Author author)
+        {
+            Author a = context.Authors.Find(author.au_id);
+            int result = -1;
+            if (a != null) { 
+                context.Authors.Remove(a); // marcado en memoria para eliminar
+                result = context.SaveChanges(); // enviar cambios a la base
+            }
+
+            return result;
+        }
+
+    }
+}
