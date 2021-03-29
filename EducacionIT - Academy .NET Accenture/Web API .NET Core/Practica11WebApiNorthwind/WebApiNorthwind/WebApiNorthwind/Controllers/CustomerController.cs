53563f36d3e15ef71aa26b6ebd828e10b860b2ba
JorgePiaggio <proto2345@protonmail.com>
2021-03-29 18:08:02 -0300

    practicas 11 y 12 webApi

---
+++ EducacionIT - Academy .NET Accenture/Web API .NET Core/Practica11WebApiNorthwind/WebApiNorthwind/WebApiNorthwind/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiNorthwind.Models;
+
+namespace WebApiNorthwind.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+        public CustomerController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Customers> Get()
+        {
+            return _context.Customers.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Customers> Get(string id)
+        {
+            var customer = _context.Customers.FirstOrDefault(x => x.CustomerId.Equals( id));
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return customer;
+        }
+
+        [HttpGet("city/{city}")]
+        public IEnumerable<Customers> GetCustomerByCity(string city)
+        {
+            IEnumerable<Customers> customers = (from c in _context.Customers where c.City == city select c).ToList();
+
+            if (customers == null)
+             {
+                 return (IEnumerable<Customers>)NotFound();
+             }
+
+            return customers;
+        }
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Customers customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObtenerCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Customers customer)
+        {
+            if( !id.Equals(customer.CustomerId) )
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<Customers> Delete (string id)
+        {
+            var customer = _context.Customers.Find(id);
+
+            if(customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+
+            return customer;
+
+        }
+    }
+}
---
+++ EducacionIT - Academy .NET Accenture/Web API .NET Core/Practica12WebApiMovie/WebApiMovie/WebApiMovie/Controllers/MovieController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiMovie.Data;
+using WebApiMovie.Models;
+
+namespace WebApiMovie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieContext _context;
+        public MovieController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _context.Movies.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get( int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            if( movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        [HttpGet("GetByDate/{date}")]
+        public IEnumerable<Movie> GetByDate(int date)
+        {
+            //  IEnumerable<Movie> movies = (from c in _context.Movies where c.ReleaseYear == date select c).ToList();
+            IEnumerable<Movie> movies = _context.Movies.Where(x => x.ReleaseYear == date).ToList();
+
+            if (movies == null)
+            {
+                return (IEnumerable<Movie>)NotFound();
+            }
+
+            return movies;
+        }
+
+
+
+    }
+}
